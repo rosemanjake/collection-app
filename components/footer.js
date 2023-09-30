4deca884a875add948c5cdd5b21d0ccc9f073b26
rosemanjake <roseman.jake@gmail.com>
2023-09-30 14:43:13 +0100

    name change

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ export default function Footer(){
   return(
     <div className={styles.container}>
       <div className={styles.innerContainer}>
-        <div className={styles.title}>By the Fire</div>
+        <div className={styles.title}>Murphy's Run</div>
         <div className={styles.content}>
           <div className={styles.links}>
             <Link href={"/"}>Home</Link>
